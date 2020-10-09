e016d6214f826904131843aeb2b566e3014e5243
jimmg35 <jim60308@gmail.com>
2020-10-09 12:58:35 +0800

    jimmg_20201009

--- scripts/main.py
+++ scripts/main.py
@@ -2,8 +2,13 @@ from module.extractor import DataExtractor
 
 
 
-DE = DataExtractor(r'D:\covid\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_US.csv')
+DE = DataExtractor()
 
-DE.load_timeseries_us()
+DE.load_timeseries_us(r'D:\covid\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_US.csv')
 DE.calculate_daily_county_confirmed()
-DE.output_daily_county_confirmed()
+#DE.output_county_confirmed()
+DE.output_province_confirmed(["Florida", "Arizona", "California", "Minnesota", "Texas"], 
+                             [10, 30, 60],
+                             [(50/255, 168/255, 82/255),
+                              (23/255, 192/255, 230/255),
+                              (230/255, 88/255, 23/255)])
--- scripts/module/extractor.py
+++ scripts/module/extractor.py
@@ -1,14 +1,17 @@
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt 
 import os
 from os import listdir
 
-
+import numpy as np
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+from matplotlib.dates import MonthLocator, DateFormatter
+import matplotlib.ticker as mticker
 
 class DataExtractor():
-    def __init__(self, data_path):
-        self.data_path = data_path
+    def __init__(self):
         self.province_to_county = {}
         self.county_confirmed = []
         self.unique_province = []
@@ -16,7 +19,7 @@ class DataExtractor():
         self.county = []
         self.timeseries = []
     
-    def load_timeseries_us(self):
+    def load_timeseries_us(self, data_path):
         
         '''
             Load .csv and require some basic info
@@ -24,7 +27,7 @@ class DataExtractor():
         '''
         
         # Load csv
-        data = pd.read_csv(self.data_path)
+        data = pd.read_csv(data_path)
         # Drop Unassigned and Out of Province
         unassigned_index_list = data[data['Admin2']=='Unassigned'].index
         outof_index_list = data[data['Admin2'].astype(str).str.contains("Out of")].index
@@ -64,7 +67,7 @@ class DataExtractor():
                 daily_confirmed = self.data[self.timeseries[i]][j] - self.data[self.timeseries[i-1]][j]
                 self.county_confirmed[j].append(daily_confirmed)
 
-    def output_daily_county_confirmed(self):
+    def output_county_confirmed(self):
         
         d = {
             "Province":self.province,
@@ -80,7 +83,69 @@ class DataExtractor():
         self.processed_data.to_csv(r'output.csv')
         print("Output Complete!")
                 
+    def output_province_confirmed(self, provinces, MAs, MAcolor):
         
+        '''
+            ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California',
+             'Colorado', 'Connecticut', 'Delaware', 'Diamond Princess', 'District of Columbia',
+             'Florida', 'Georgia', 'Grand Princess', 'Guam', 'Hawaii', 'Idaho', 'Illinois',
+             'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+             'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+             'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+             'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
+             'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+             'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+             'Virgin Islands', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin','Wyoming']
+        '''
+        
+        for province in provinces: 
+            
+            confirmed_timeseries = []
+            total_province_confirmed = 0
+            # Get specific province and transpose
+            interval_county_confirmed = []
+            interval_county_confirmed = self.get_province_data(province)
+            a = np.array(interval_county_confirmed)
+            
+            # Calculate daily confirmed
+            for i in range(0, len(self.timeseries)):
+                total_confirmed_per_d = 0
+                for j in a.T.tolist()[i]:
+                    total_confirmed_per_d += j
+                confirmed_timeseries.append(total_confirmed_per_d)
+                
+            # Plot the daily confirmed
+            d_color = (101/255, 107/255, 255/255, 0.9)
+            fig, ax = plt.subplots(figsize=(16, 8))
+            ax.plot(self.timeseries, confirmed_timeseries, label="daily confirmed", color=(190/255, 190/255, 190/255))
+            
+            
+            for m in range(0, len(MAs)):
+                ma_list = []
+                for i in range(MAs[m], len(self.timeseries)):
+                    d = 0
+                    for j in range(1, MAs[m]+1):
+                        for k in a.T.tolist()[i-j]:
+                            d += k
+                    ma_list.append(int(d / MAs[m]))
+                ax.plot(ma_list, label="{}MA".format(MAs[m]), color=MAcolor[m])
+                
+            
+            
+            plt.xticks(np.arange(10, len(self.timeseries), 31.0), rotation=40)
+            plt.title("Daily confirmed trend of {}".format(province))
+            plt.legend()
+            plt.show()   
+            
+    def get_province_data(self, province):          
+        interval_county_confirmed = []
+        for i in range(0, len(self.province)):
+            if self.province[i] == province:
+                interval_county_confirmed.append(self.county_confirmed[i])
+        return interval_county_confirmed
+                    
+                    
+                
         
         
     
