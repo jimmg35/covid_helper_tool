2d40098312b360b56de9ed9dd937394c3b4a2cb2
jimmg35 <jim60308@gmail.com>
2020-10-10 16:28:07 +0800

    jimmg35_20201010

--- scripts/main.py
+++ scripts/main.py
@@ -2,21 +2,42 @@ from module.extractor import DataExtractor
 
 
 
+provinces = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California',
+             'Colorado', 'Connecticut', 'Delaware', 'Diamond Princess', 'District of Columbia',
+             'Florida', 'Georgia', 'Grand Princess', 'Guam', 'Hawaii', 'Idaho', 'Illinois',
+             'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+             'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+             'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+             'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
+             'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+             'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+             'Virgin Islands', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin','Wyoming']
+
+
 DE = DataExtractor()
 DE.load_timeseries_us(r'D:\covid\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_US.csv')
+
 
 # Must execute this line in order to get basic info.
 DE.calculate_daily_county_confirmed()
 
+
 # Output daily confirmed csv(not necessary)
 #DE.output_county_confirmed()
 
+
 # Output visualized data, can output multiple province at once
 # and you can set the scale of MA and its color
-DE.output_province_confirmed(
-                             provinces=["Florida", "Arizona", "California", "Minnesota", "Texas"], 
-                             MAs=[10, 30, 60],
-                             MAcolor=[(50/255, 168/255, 82/255),
-                              (23/255, 192/255, 230/255),
-                              (230/255, 88/255, 23/255)],
-                             output_folder=r'D:\covid\daily_province_confirmed_chart')
+# DE.province_confirmed(
+#                    provinces=provinces, 
+#                    MAs=[10, 30, 60],
+#                    MAcolor=[(50/255, 168/255, 82/255),
+#                    (23/255, 192/255, 230/255),
+#                    (230/255, 88/255, 23/255)],
+#                    output_folder=r'D:\covid\daily_province_confirmed_chart')
+
+
+DE.province_bollinger(provinces, 
+                      20, 
+                      r'D:\covid\bollinger_confirmed_chart')
+
--- scripts/module/extractor.py
+++ scripts/module/extractor.py
@@ -83,7 +83,7 @@ class DataExtractor():
         self.processed_data.to_csv(r'output.csv')
         print("Output Complete!")
                 
-    def output_province_confirmed(self, provinces, MAs, MAcolor, output_folder):
+    def province_confirmed(self, provinces, MAs, MAcolor, output_folder):
         
         '''
             ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California',
@@ -121,7 +121,7 @@ class DataExtractor():
             
             # Calculate MA
             for m in range(0, len(MAs)):
-                ma_list = []
+                ma_list = [0 for d in range(0, MAs[m])]
                 for i in range(MAs[m], len(self.timeseries)):
                     d = 0
                     for j in range(1, MAs[m]+1):
@@ -131,9 +131,86 @@ class DataExtractor():
                 ax.plot(ma_list, label="{}MA".format(MAs[m]), color=MAcolor[m])
                 
             plt.xticks(np.arange(10, len(self.timeseries), 31.0), rotation=40)
-            plt.title("Daily confirmed trend of {}".format(province))
+            plt.title("Daily confirmed trend of {} ({}~{})".format(province, self.timeseries[0], self.timeseries[len(self.timeseries)-1]), fontdict = {'fontsize' : 20})
             plt.legend()
             plt.savefig(os.path.join(output_folder, province+"_confirmed.png"))   
+            print("{} done!".format(province))
+            
+    def province_bollinger(self, provinces, MA, output_folder):
+        
+        '''
+            ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California',
+             'Colorado', 'Connecticut', 'Delaware', 'Diamond Princess', 'District of Columbia',
+             'Florida', 'Georgia', 'Grand Princess', 'Guam', 'Hawaii', 'Idaho', 'Illinois',
+             'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+             'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+             'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+             'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
+             'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+             'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+             'Virgin Islands', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin','Wyoming']
+        '''
+        
+        for province in provinces:
+            
+            confirmed_timeseries = []
+            total_province_confirmed = 0
+            # Get specific province and transpose
+            interval_county_confirmed = []
+            interval_county_confirmed = self.get_province_data(province)
+            a = np.array(interval_county_confirmed)
+            
+            # Calculate daily confirmed
+            for i in range(0, len(self.timeseries)):
+                total_confirmed_per_d = 0
+                for j in a.T.tolist()[i]:
+                    total_confirmed_per_d += j
+                confirmed_timeseries.append(total_confirmed_per_d)
+                
+            # Plot the daily confirmed
+            d_color = (101/255, 107/255, 255/255, 0.9)
+            fig, ax = plt.subplots(figsize=(16, 8))
+            ax.plot(self.timeseries, confirmed_timeseries, label="daily confirmed", color=(190/255, 190/255, 190/255))
+            
+            ma_list = [0 for d in range(0, MA)]
+            upper_list = [0 for d in range(0, MA)]
+            lower_list = [0 for d in range(0, MA)]
+            for i in range(MA, len(self.timeseries)):
+                # Calculate Average
+                d = 0
+                for j in range(1, MA+1):
+                    for k in a.T.tolist()[i-j]:
+                        d += k
+
+                # Calculate past MA days's confirmed cases
+                average = int(d / MA)
+                past_day = []
+                for j in range(1, MA+1):
+                    past_day_count = 0
+                    for k in a.T.tolist()[i-j]:
+                        past_day_count += k
+                    past_day.append(past_day_count)
+                
+                # Calculate Standard deviation
+                total_DFM = 0
+                for j in past_day:
+                    total_DFM += (j - average)*(j - average)
+                std = (total_DFM / MA) ** 0.5
+                    
+                ma_list.append(average)
+                upper_list.append(average + 2 * std)
+                lower_list.append(average - 2 * std)
+            
+            # Plot MA and bollinger band
+            ax.plot(ma_list, label="{}MA".format(MA) ,color=(23/255, 192/255, 230/255))
+            ax.plot(upper_list, label="{}MA+2std".format(MA) ,color=(209/255, 104/255, 33/255))
+            ax.plot(lower_list, label="{}MA-2std".format(MA) ,color=(209/255, 104/255, 33/255))
+            
+            plt.xticks(np.arange(10, len(self.timeseries), 31.0), rotation=40)
+            plt.title("Bollinger band of {} ({}~{})".format(province, self.timeseries[0], self.timeseries[len(self.timeseries)-1]), fontdict = {'fontsize' : 20})
+            plt.legend()
+            plt.savefig(os.path.join(output_folder, province+"_bollinger.png"))
+            print("{} done!".format(province))  
             
     def get_province_data(self, province):          
         interval_county_confirmed = []
